8296810c01aac86b034865bebedf161067a18499
Lim Sungguk <limasdf@gmail.com>
2013-02-06 18:59:54 +0900

    Update beagle-term/js/connect_dialog.js
    handle serial functions at background js

--- beagle-term/js/connect_dialog.js
+++ beagle-term/js/connect_dialog.js
@@ -12,24 +12,26 @@ ConnectDialog.show = function(onComplete) {
   var $bitratePicker = $('#bitrate-picker');
 
   // Build port picker
-  serial_lib.getPorts(function(ports) {
-    var eligiblePorts = ports.filter(function(port) {
-      return !port.match(/[Bb]luetooth/);
+  chrome.runtime.getBackgroundPage(function(bgPage) {
+    bgPage.serial_lib.getPorts(function(ports) {
+        var eligiblePorts = ports.filter(function(port) {
+          return !port.match(/[Bb]luetooth/);
+        });
+
+        if (eligiblePorts.length > 0) {
+          eligiblePorts.forEach(function(port) {
+            var $option = $('<option>' + port + '</option>').attr('value', port);
+            $portPicker.append($option);
+          });
+
+          // Show setup dialog
+          $('#connect-dialog-trigger').click();
+
+        } else {
+          // Show error dialog      
+          MessageDialog.show('Could not find serial device. Please check your serial connection and try again.');
+        }
     });
-
-    if (eligiblePorts.length > 0) {
-      eligiblePorts.forEach(function(port) {
-        var $option = $('<option>' + port + '</option>').attr('value', port);
-        $portPicker.append($option);
-      });
-
-      // Show setup dialog
-      $('#connect-dialog-trigger').click();
-
-    } else {
-      // Show error dialog      
-      MessageDialog.show('Could not find serial device. Please check your serial connection and try again.');
-    }
   });
 
   $('#connect').click(function() {
