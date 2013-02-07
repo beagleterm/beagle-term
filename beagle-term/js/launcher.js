c0558bc00f071197babe7f17797040aacf8fab83
Lim Sungguk <limasdf@gmail.com>
2013-02-07 21:35:55 +0900

    Update beagle-term/js/launcher.js
    close serial port when window is closed

--- beagle-term/js/launcher.js
+++ beagle-term/js/launcher.js
@@ -1,13 +1,13 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('html/beagle.html', {
-    width: 1024,
-    height: 768
-  });
+    chrome.app.window.create('html/beagle.html', {
+        width: 1024,
+        height: 768
+    }, function(win) {
+        win.onClosed.addListener(function() {
+            console.log("called at onClosed");
+            serial_lib.closeSerial(function() {
+                console.log("close successfully");
+            });
+        });
+    });
 })
-
-chrome.runtime.onSuspend.addListener(function() { 
-  console.log("called at chrome.runtime.onSuspend.addListener");
-    serial_lib.closeSerial(function(){
-      console.log("close successfully");
-    })
-});
