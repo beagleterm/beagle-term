2c04f033ce7e675f6120e305bcd944dbc0ed324d
Lim Sungguk <limasdf@gmail.com>
2013-03-28 17:57:15 +0900

    Minor Refactoring
    Minor refactoring to handle bitrate by chrome.storage

--- beagle-term/js/connect_dialog.js
+++ beagle-term/js/connect_dialog.js
@@ -6,51 +6,37 @@ ConnectDialog.show = function(onComplete) {
   lib.ensureRuntimeDependencies();
   $('a[rel*=leanModal]').leanModal({closeButton: '#connect'});
   
-  var port = null;
-  var bitrate = 9600;
   var $portPicker = $('#port-picker');
   var $bitratePicker = $('#bitrate-picker');
-  var storage = chrome.storage.local;
-  
-  var BITRATE_KEY = 'bit_rate';
-  var obj = {};
-  
-  storage.get(BITRATE_KEY,function(result){
-    console.log('have;? : ' + result.bit_rate);
-    if (result.bitrate != '') { //if not empty or not in a list of bitrate
-        $('#bitrate-picker').val(result.bit_rate);
-    }
-  });
+
+  this.loadBitrate();
   
   // Build port picker
   chrome.runtime.getBackgroundPage(function(bgPage) {
     bgPage.serial_lib.getPorts(function(ports) {
-        var eligiblePorts = ports.filter(function(port) {
-          return !port.match(/[Bb]luetooth/);
+      var eligiblePorts = ports.filter(function(port) {
+        return !port.match(/[Bb]luetooth/);
+      });
+
+      if (eligiblePorts.length > 0) {
+        eligiblePorts.forEach(function(port) {
+          var $option = $('<option>' + port + '</option>').attr('value', port);
+          $portPicker.append($option);
         });
 
-        if (eligiblePorts.length > 0) {
-          eligiblePorts.forEach(function(port) {
-            var $option = $('<option>' + port + '</option>').attr('value', port);
-            $portPicker.append($option);
-          });
+        // Show setup dialog
+        $('#connect-dialog-trigger').click();
 
-          // Show setup dialog
-          $('#connect-dialog-trigger').click();
-
-        } else {
-          // Show error dialog
-          MessageDialog.show('Could not find serial device. Please check your serial connection and try again.');
-        }
+      } else {
+        // Show error dialog
+        MessageDialog.show('Could not find serial device. Please check your serial connection and try again.');
+      }
     });
   });
 
   $('#connect').click(function() {
     if (onComplete) {
-      // save bitrate
-      obj[BITRATE_KEY] = $bitratePicker.val();
-      storage.set(obj);
-
+      ConnectDialog.saveBitrate($bitratePicker.val());
       onComplete({
         port: $portPicker.val(),
         bitrate: $bitratePicker.val()
@@ -59,3 +45,18 @@ ConnectDialog.show = function(onComplete) {
   });
 };
 
+ConnectDialog.saveBitrate = function(bitrate) {
+  var BITRATE_KEY = 'bit_rate'; //when you change this value, you also must check loadBitrate();
+  var obj = {};
+  obj[BITRATE_KEY] = bitrate;
+  chrome.storage.local.set(obj);
+};
+
+ConnectDialog.loadBitrate = function() {
+  var BITRATE_KEY = 'bit_rate'; //when you change this value, you also must check saveBitrate();
+  chrome.storage.local.get(BITRATE_KEY,function(result){
+    if (result.bit_rate !== undefined) {
+      $('#bitrate-picker').val(result[BITRATE_KEY]);
+    }
+  });
+};
