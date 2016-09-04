8daffeb1f95614a78879613e1832fe942054f245
Tyler Hughes <thughes@randomlyknighted.com>
2016-09-04 16:22:33 -0500

    Removed Polymer dependency

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    $("#settingsModal").modal("show");
+}, false);
+
+/*
+$(window).load(function(){
+    $("#settingsModal").modal("show");
+});
+*/
