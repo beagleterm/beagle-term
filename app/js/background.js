cc435c75d0975ac01895861d8dbc2916b8904a99
Sungguk Lim <limasdf@gmail.com>
2016-09-12 16:41:53 +0900

    Update background.js

--- app/js/background.js
+++ app/js/background.js
@@ -1,5 +1,6 @@
-// Copyright 2015 The Beagle term Authors. All rights reserved.
-// Use of this source code is governed by MIT LICENSE.
+// Copyright (c) 2012, Sungguk Lim. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 chrome.app.runtime.onLaunched.addListener(function() {
   new BeagleWindow();
