26fac7bc2ea36b24d9799229248a355d7184b8a7
Tyler Hughes <thughes@randomlyknighted.com>
2016-09-12 22:42:36 -0500

    Added .jscsrc file to help enforce the Google style guide

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -2,26 +2,34 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: "<json:package.json>",
+    pkg: '<json:package.json>',
     jshint: {
       all: {
         src: [
-          "js/index.js", "js/main.js"
+          'js/index.js', 'js/main.js'
         ]
       }
     },
+    jscs: {
+      src: ['Gruntfile.js', 'js/*.js'],
+      options: {
+        config: '.jscsrc',
+        fix: true
+      }
+    },
     watch: {
-        files: ["js/index.js", "js/main.js"],
-        tasks: ["jshint"]
+        files: ['js/index.js', 'js/main.js'],
+        tasks: ['jshint', 'jscs']
       }
   });
 
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jscs');
 
   // Default task.
-  grunt.registerTask("default", ["jshint", "watch"]);
+  grunt.registerTask('default', ['jshint', 'jscs', 'watch']);
 
   // Test task
-  grunt.registerTask("test", ["jshint"]);
+  grunt.registerTask('test', ['jshint', 'jscs']);
 };
