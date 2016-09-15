4882e4ab74fb191a3f9b4e3dfe4b394d14a75d6e
LIMSUNG GUK <limasdf@gmail.com>
2016-09-15 22:14:36 +0900

    packing script will copy files and create archive

--- tools/packing.py
+++ tools/packing.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import shutil
 import subprocess
 from contextlib import contextmanager
 
@@ -12,15 +13,41 @@ def pushd(path):
   os.chdir(currentDir)
 
 def installDependencies():
-  subprocess.check_call(['npm', 'install'])
-  subprocess.check_call(['bower', 'install'])
+  with pushd('app'):
+    subprocess.check_call(['bower', 'install'])
+
+def copy(src, des):
+  if os.path.isdir(src):
+    shutil.copytree(src, des)
+  else:
+    if not os.path.exists(os.path.dirname(des)):
+      os.makedirs(os.path.dirname(des))
+    shutil.copy2(src, des)
+
+def zip(destFile):
+  os.chdir('deploy')
+  subprocess.check_call(['zip', destFile, '.', '-qr', '-x', '.*'])
 
 def copyDeployFiles():
   os.makedirs('deploy')
+  source_dir = os.path.join(os.getcwd(), 'app')
+  target_dir = os.path.join(os.getcwd(), 'deploy')
+
+  copy_list = ['index.html', 'manifest.json', 'js/index.js', 'js/background.js', 'js/lib/hterm_all.min.js',
+                'css/main.css', 'images', 'bower_components/jquery/dist/jquery.min.js',
+               'bower_components/bootstrap/dist/js/bootstrap.min.js',
+               'bower_components/bootstrap/dist/css/bootstrap.min.css']
+
+  for single_file in copy_list:
+    source_path = os.path.join(source_dir, single_file)
+    target_path = os.path.join(target_dir, single_file)
+    copy(source_path, target_path)
 
 def main():
-  with pushd('app'):
-    installDependencies()
+  installDependencies()
+  copyDeployFiles()
+  # TODO: Minify js/index.js, js/background.js
+  zip('build.zip')
 
 if __name__ == '__main__':
   main()
